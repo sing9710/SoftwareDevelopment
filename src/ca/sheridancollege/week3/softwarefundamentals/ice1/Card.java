a7773f67ac40c9cf5f3eb1c6d404fc18cf570e5c
sing9710 <sing9710@sheridancollege.ca>
2020-01-23 15:25:21 -0500

    Changed author name.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author Megha Patel
+ * @author Karandeep Singh
  */
 public class Card {
 
