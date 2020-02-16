31321ed8d52bbfb788e95cdf49471266db099797
Abhijeetawal <60014174+Abhijeetawal@users.noreply.github.com>
2020-02-16 13:31:47 -0500

    hey baby

--- src/softwareweek1/Studentlist.java
+++ src/softwareweek1/Studentlist.java
@@ -8,7 +8,7 @@ public class Studentlist {
         int i;
         Student[] s1 = new Student[3];
         Student s2 = new Student();
-        s2.setYesr(123);
+        s2.setYesr(123748787898498);
         System.out.println("name " + s2.getYesr());
         Scanner sc = new Scanner(System.in);
         for (i = 1; i < s1.length; i++) {
